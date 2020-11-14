f14d2312ff3f94262fe7cee7c7f4ad3a1ab44348
brucism <73067641+brucism@users.noreply.github.com>
2020-11-14 09:18:58 +0800

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		Cat cat = new Cat();
+		cat.say();
+		dog.say();
+		cat.eat();
+		dog.run();
+		dog.bark();
+		if (cat instanceof BarkAbility) {
+			((BarkAbility) cat).bark();
+		}
+		if (dog instanceof BarkAbility) {
+			((BarkAbility) dog).bark();
+		}
+		String name = "hello";
+		int age1 = 3;
+
+
+	}
+}
